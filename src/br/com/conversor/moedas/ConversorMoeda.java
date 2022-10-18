4949852eead727e90639316c5615e12448f46836
guipmenezes <guimenezesunb@gmail.com>
2022-10-18 13:38:31 -0300

    second commit of the conversor de moeda

---
+++ src/br/com/conversor/moedas/ConversorMoeda.java
@@ -0,0 +1,33 @@
+package br.com.conversor.moedas;
+import javax.swing.JOptionPane;
+
+public class ConversorMoeda {
+	
+	public static void main(String[] args) {
+		
+		FuncaoConverter converter = new FuncaoConverter();
+		
+		while(true) {
+			String options = JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu", JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Conversor de moeda", "Conversor de temperatura"}, "Escolha").toString();
+			switch(options)  {
+				case "Conversor de moeda":
+					String input = JOptionPane.showInputDialog("Insira um valor em reais");
+					double valorRecebido = Double.parseDouble(input);
+					String optionsMoeda = JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu", JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Reais para Dolar", "Reais para Euro", "Reais para Libra Esterlina", "Reais para Peso Argentino", "Reais para Peso Chileno"}, "Escolha").toString();
+					switch(optionsMoeda) {
+						case "Reais para Dolar":		
+							converter.ConverterReaisParaDolar(valorRecebido);
+					}
+					switch(optionsMoeda) {
+						case "Reais para Euro":
+							converter.ConverterReaisParaEuro(valorRecebido);
+					}
+					switch(optionsMoeda) {
+						
+					}
+					
+					
+			}
+		}
+	}
+}
---
+++ src/br/com/conversor/moedas/FuncaoConverter.java
@@ -0,0 +1,36 @@
+package br.com.conversor.moedas;
+
+import javax.swing.JOptionPane;
+
+public class FuncaoConverter {
+
+	public void ConverterReaisParaDolar(double valorRecebido) {
+		double moedaDolar = valorRecebido / 5.30;
+		moedaDolar = (double) Math.round(moedaDolar *100d)/100;
+		JOptionPane.showMessageDialog(null, "Você tem $" + moedaDolar + " Dolares");
+	}
+	
+	public void ConverterReaisParaEuro(double valorRecebido) {
+		double moedaEuro = valorRecebido / 5.21;
+		moedaEuro = (double) Math.round(moedaEuro * 100d)/100;
+		JOptionPane.showMessageDialog(null, "Você tem " + moedaEuro + " Euros");
+	}
+	
+	public void ConverterReaisParaLibras(double valorRecebido) {
+		double moedaLibras = valorRecebido / 5.96;
+		moedaLibras = (double) Math.round(moedaLibras * 100d)/100;
+		JOptionPane.showMessageDialog(null, "Você tem " + moedaLibras + " Libras");
+	}
+	
+	public void ConverterReaisParaPA(double valorRecebido) {
+		double moedaPA = valorRecebido / 0.035;
+		moedaPA = (double) Math.round(moedaPA * 100d)/100;
+		JOptionPane.showMessageDialog(null, "Você tem " + moedaPA + " Libras");
+	}
+	
+	public void ConverterReaisParaPC(double valorRecebido) {
+		double moedaPC = valorRecebido / 5.96;
+		moedaPC = (double) Math.round(moedaPC * 100d)/100;
+		JOptionPane.showMessageDialog(null, "Você tem " + moedaPC + " Libras");
+	}
+}
